2526f6be421283bb485b7603e5b953150a99dd53
unknown <sergio_ludke1@hotmail.com>
2025-11-13 15:48:15 -0300

    Inicializando projeto com base e dependencias

---
+++ src/test/java/com/dt/delivery_tracker/TestDeliveryTrackerApplication.java
@@ -0,0 +1,11 @@
+package com.dt.delivery_tracker;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestDeliveryTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(DeliveryTrackerApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
